35d31e40f59e700843cf7429af0f979a04d3f103
Lars Frogner <lars_frogner@hotmail.com>
2022-08-05 23:53:05 +0200

    Utilize that `nalgebra` matrices implement `Pod`

--- src/rendering/camera.rs
+++ src/rendering/camera.rs
@@ -5,8 +5,7 @@ use crate::rendering::{
     buffer::{BufferableUniform, UniformBuffer},
     CoreRenderingSystem,
 };
-use bytemuck::{Pod, Zeroable};
-use nalgebra::{Matrix4, Projective3};
+use nalgebra::Projective3;
 
 /// Owner and manager of render data for cameras.
 #[derive(Debug)]
@@ -14,14 +13,6 @@ pub struct CameraRenderDataManager {
     transform_buffer: UniformBuffer,
     transform_bind_group_layout: wgpu::BindGroupLayout,
     transform_bind_group: wgpu::BindGroup,
-}
-
-/// Representation of a view projection transform
-/// as a nested slice of matrix elements.
-#[repr(C)]
-#[derive(Copy, Clone, Debug, Zeroable, Pod)]
-pub struct RawViewProjectionTransform {
-    view_projection_matrix: [[f32; 4]; 4],
 }
 
 impl CameraRenderDataManager {
@@ -33,7 +24,7 @@ impl CameraRenderDataManager {
         label: &str,
     ) -> Self {
         let view_projection_transform = camera.compute_view_projection_transform();
-        Self::new(core_system, &view_projection_transform, label)
+        Self::new(core_system, view_projection_transform, label)
     }
 
     /// Ensures that the render data is in sync with the view
@@ -45,7 +36,7 @@ impl CameraRenderDataManager {
     ) {
         if camera.view_projection_transform_changed() {
             let view_projection_transform = camera.compute_view_projection_transform();
-            self.sync_render_data(core_system, &view_projection_transform);
+            self.sync_render_data(core_system, view_projection_transform);
             camera.reset_view_projection_change_tracking();
         }
     }
@@ -72,11 +63,10 @@ impl CameraRenderDataManager {
     /// from the given view projection transform.
     fn new(
         core_system: &CoreRenderingSystem,
-        view_projection_transform: &Projective3<f32>,
+        view_projection_transform: Projective3<f32>,
         label: &str,
     ) -> Self {
-        let raw_transform = RawViewProjectionTransform::from_transform(view_projection_transform);
-        let transform_buffer = UniformBuffer::new(core_system, &[raw_transform], label);
+        let transform_buffer = UniformBuffer::new(core_system, &[view_projection_transform], label);
 
         let (transform_bind_group, transform_bind_group_layout) =
             transform_buffer.create_bind_group_and_layout(core_system.device());
@@ -91,33 +81,17 @@ impl CameraRenderDataManager {
     fn sync_render_data(
         &mut self,
         core_system: &CoreRenderingSystem,
-        view_projection_transform: &Projective3<f32>,
+        view_projection_transform: Projective3<f32>,
     ) {
-        let raw_transform = RawViewProjectionTransform::from_transform(view_projection_transform);
-        self.transform_buffer
-            .queue_update_of_uniforms(core_system, 0, &[raw_transform]);
+        self.transform_buffer.queue_update_of_uniforms(
+            core_system,
+            0,
+            &[view_projection_transform],
+        );
     }
 }
 
-impl RawViewProjectionTransform {
-    /// Creates a new raw transform matrix from the given [`Projective3`].
-    fn from_transform(view_projection_transform: &Projective3<f32>) -> Self {
-        Self::from_matrix(view_projection_transform.matrix())
-    }
-
-    /// Creates a new raw transform matrix from the given [`Matrix4`].
-    fn from_matrix(view_projection_matrix: &Matrix4<f32>) -> Self {
-        Self::new(*view_projection_matrix.as_ref())
-    }
-
-    fn new(view_projection_matrix: [[f32; 4]; 4]) -> Self {
-        Self {
-            view_projection_matrix,
-        }
-    }
-}
-
-impl BufferableUniform for RawViewProjectionTransform {
+impl BufferableUniform for Projective3<f32> {
     const BIND_GROUP_LAYOUT_DESCRIPTOR: wgpu::BindGroupLayoutDescriptor<'static> =
         wgpu::BindGroupLayoutDescriptor {
             entries: &[wgpu::BindGroupLayoutEntry {
