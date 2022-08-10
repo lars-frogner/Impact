3d3ddb8307de3260de878f67c8e6a046c60358c3
Lars Frogner <lars_frogner@hotmail.com>
2022-08-11 00:29:08 +0200

    Remove unneccesary trait bound

--- src/num.rs
+++ src/num.rs
@@ -4,10 +4,7 @@ use nalgebra as na;
 use num_traits as nt;
 
 /// Gathers traits useful for working with generic floating point types.
-pub trait Float:
-    nt::Float + nt::FloatConst + nt::FromPrimitive + na::RealField + na::Scalar
-{
-}
+pub trait Float: Copy + nt::FloatConst + nt::FromPrimitive + na::RealField + na::Scalar {}
 
 impl Float for f32 {}
 impl Float for f64 {}
