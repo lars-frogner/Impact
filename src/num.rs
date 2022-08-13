40c64495ef5df8de3ba7bc0b820d69baface1cae
Lars Frogner <lars_frogner@hotmail.com>
2022-08-14 00:13:56 +0200

    Add useful associated constants for float trait

--- src/num.rs
+++ src/num.rs
@@ -4,7 +4,21 @@ use nalgebra as na;
 use num_traits as nt;
 
 /// Gathers traits useful for working with generic floating point types.
-pub trait Float: Copy + nt::FloatConst + nt::FromPrimitive + na::RealField + na::Scalar {}
+pub trait Float: Copy + nt::FloatConst + nt::FromPrimitive + na::RealField + na::Scalar {
+    const ZERO: Self;
+    const ONE: Self;
+    const NEG_ONE: Self;
+}
 
-impl Float for f32 {}
-impl Float for f64 {}
+macro_rules! impl_float {
+    ($f:ty) => {
+        impl Float for $f {
+            const ZERO: Self = 0.0;
+            const ONE: Self = 1.0;
+            const NEG_ONE: Self = -1.0;
+        }
+    };
+}
+
+impl_float!(f32);
+impl_float!(f64);
