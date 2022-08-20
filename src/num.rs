6b2e93a779f42e714db7724714cd7ee904a0e7c1
Lars Frogner <lars_frogner@hotmail.com>
2022-08-21 00:12:14 +0200

    Add useful associated constants for float trait

--- src/num.rs
+++ src/num.rs
@@ -8,6 +8,8 @@ pub trait Float: Copy + nt::FloatConst + nt::FromPrimitive + na::RealField + na:
     const ZERO: Self;
     const ONE: Self;
     const NEG_ONE: Self;
+    const MIN: Self;
+    const MAX: Self;
 }
 
 macro_rules! impl_float {
@@ -16,6 +18,8 @@ macro_rules! impl_float {
             const ZERO: Self = 0.0;
             const ONE: Self = 1.0;
             const NEG_ONE: Self = -1.0;
+            const MIN: Self = Self::MIN;
+            const MAX: Self = Self::MAX;
         }
     };
 }
