516b1eaf46a44786f0504bea6f848e50ac69390a
Lars Frogner <lars_frogner@hotmail.com>
2022-08-21 21:00:42 +0200

    Add unit tests

--- src/util.rs
+++ src/util.rs
@@ -136,3 +136,143 @@ impl<T> VecWithFreeList<T> {
         self.free_list.push_back(idx);
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn creating_vec_works() {
+        let vec = VecWithFreeList::<f32>::new();
+        assert_eq!(vec.n_elements(), 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn demanding_element_from_empty_vec_fails() {
+        let vec = VecWithFreeList::<f32>::new();
+        vec.element(0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn demanding_mutable_element_from_empty_vec_fails() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        vec.element_mut(0);
+    }
+
+    #[test]
+    fn requesting_element_from_empty_vec_gives_none() {
+        let vec = VecWithFreeList::<f32>::new();
+        assert!(vec.get_element(0).is_none());
+    }
+
+    #[test]
+    fn requesting_mutable_element_from_empty_vec_gives_none() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        assert!(vec.get_element_mut(0).is_none());
+    }
+
+    #[test]
+    fn adding_to_empty_vec_works() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        let idx = vec.add_element(1.0);
+
+        assert_eq!(idx, 0);
+
+        assert_eq!(vec.n_elements(), 1);
+
+        assert_eq!(*vec.element(idx), 1.0);
+        assert_eq!(*vec.element_mut(idx), 1.0);
+
+        assert_eq!(vec.get_element(idx), Some(&1.0));
+        assert_eq!(vec.get_element_mut(idx), Some(&mut 1.0));
+
+        assert!(vec.has_element_at_idx(idx));
+    }
+
+    #[test]
+    #[should_panic]
+    fn freeing_out_of_bounds_idx_fails() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        vec.free_element_at_idx(0);
+    }
+
+    #[test]
+    fn freeing_only_element_in_vec_works() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        let idx = vec.add_element(1.0);
+        vec.free_element_at_idx(idx);
+
+        assert_eq!(vec.n_elements(), 0);
+        assert_eq!(vec.get_element(idx), None);
+        assert_eq!(vec.get_element_mut(idx), None);
+        assert!(!vec.has_element_at_idx(idx));
+    }
+
+    #[test]
+    #[should_panic]
+    fn demanding_freed_element_fails() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        let idx = vec.add_element(1.0);
+        vec.free_element_at_idx(idx);
+        vec.element(idx);
+    }
+
+    #[test]
+    #[should_panic]
+    fn demanding_freed_mutable_element_fails() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        let idx = vec.add_element(1.0);
+        vec.free_element_at_idx(idx);
+        vec.element_mut(idx);
+    }
+
+    #[test]
+    fn requesting_freed_element_gives_none() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        let idx = vec.add_element(1.0);
+        vec.free_element_at_idx(idx);
+        assert!(vec.get_element(idx).is_none());
+    }
+
+    #[test]
+    fn requesting_freed_mutable_element_gives_none() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        let idx = vec.add_element(1.0);
+        vec.free_element_at_idx(idx);
+        assert!(vec.get_element_mut(idx).is_none());
+    }
+
+    #[test]
+    #[should_panic]
+    fn freeing_freed_element_fails() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        let idx = vec.add_element(1.0);
+        vec.free_element_at_idx(idx);
+        vec.free_element_at_idx(idx);
+    }
+
+    #[test]
+    fn adding_after_free_uses_free_location() {
+        let mut vec = VecWithFreeList::<f32>::new();
+        let first_idx = vec.add_element(0.0);
+        assert_eq!(first_idx, 0);
+        assert_eq!(vec.n_elements(), 1);
+
+        let second_idx = vec.add_element(1.0);
+        assert_eq!(second_idx, 1);
+        assert_eq!(vec.n_elements(), 2);
+
+        vec.free_element_at_idx(first_idx);
+        assert_eq!(vec.n_elements(), 1);
+
+        let third_idx = vec.add_element(2.0);
+        assert_eq!(third_idx, first_idx);
+        assert_eq!(vec.n_elements(), 2);
+
+        let fourth_idx = vec.add_element(3.0);
+        assert_eq!(fourth_idx, 2);
+        assert_eq!(vec.n_elements(), 3);
+    }
+}
