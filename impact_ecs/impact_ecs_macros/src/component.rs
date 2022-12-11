c86ff417ad73e1b765846cd4954fe7b6a1cdaf4f
Lars Frogner <lars_frogner@hotmail.com>
2022-12-11 14:05:31 +0100

    Make `Component` derive macro also work for generic types

--- impact_ecs/impact_ecs_macros/src/component.rs
+++ impact_ecs/impact_ecs_macros/src/component.rs
@@ -6,14 +6,32 @@ use syn::DeriveInput;
 
 pub(crate) fn impl_component(input: DeriveInput, crate_root: &Ident) -> TokenStream {
     let type_name = &input.ident;
-    quote! {
-        impl #crate_root::component::Component for #type_name {
-            fn component_bytes(&self) -> #crate_root::component::ComponentByteView {
-                #crate_root::component::ComponentByteView::new(
-                    Self::component_id(),
-                    ::std::mem::size_of::<#type_name>(),
-                    bytemuck::bytes_of(self),
-                )
+    let generics = input.generics;
+    if generics.params.is_empty() {
+        // Non-generic type
+        quote! {
+            impl #crate_root::component::Component for #type_name {
+                fn component_bytes(&self) -> #crate_root::component::ComponentByteView {
+                    #crate_root::component::ComponentByteView::new(
+                        Self::component_id(),
+                        ::std::mem::size_of::<#type_name>(),
+                        bytemuck::bytes_of(self),
+                    )
+                }
+            }
+        }
+    } else {
+        // Generic type
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+        quote! {
+            impl #impl_generics #crate_root::component::Component for #type_name #ty_generics #where_clause {
+                fn component_bytes(&self) -> #crate_root::component::ComponentByteView {
+                    #crate_root::component::ComponentByteView::new(
+                        Self::component_id(),
+                        ::std::mem::size_of::<#type_name #ty_generics>(),
+                        bytemuck::bytes_of(self),
+                    )
+                }
             }
         }
     }
