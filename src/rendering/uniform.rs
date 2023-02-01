b424f5ac32d2e156b5a950b6c0352f744f903c17
Lars Frogner <lars_frogner@hotmail.com>
2023-02-01 22:52:01 +0100

    Use counted render buffer for uniforms

--- src/rendering/uniform.rs
+++ src/rendering/uniform.rs
@@ -3,7 +3,7 @@
 use crate::{
     geometry::{CollectionChange, UniformBuffer},
     rendering::{
-        buffer::{self, RenderBuffer, RenderBufferType, UniformBufferable},
+        buffer::{Count, CountedRenderBuffer, UniformBufferable},
         CoreRenderingSystem,
     },
 };
@@ -13,7 +13,7 @@ use std::{fmt::Debug, hash::Hash, mem};
 /// Owner and manager of a render buffer for uniforms.
 #[derive(Debug)]
 pub struct UniformRenderBufferManager {
-    uniform_render_buffer: RenderBuffer,
+    uniform_render_buffer: CountedRenderBuffer,
     uniform_id: ConstStringHash64,
 }
 
@@ -30,15 +30,10 @@ impl UniformRenderBufferManager {
     {
         let uniform_id = U::ID;
 
-        let n_valid_bytes = mem::size_of::<U>()
-            .checked_mul(uniform_buffer.n_valid_uniforms())
-            .unwrap();
-
-        let uniform_render_buffer = RenderBuffer::new(
+        let uniform_render_buffer = CountedRenderBuffer::new_uniform_buffer(
             core_system,
-            RenderBufferType::Uniform,
-            bytemuck::cast_slice(uniform_buffer.raw_buffer()),
-            n_valid_bytes,
+            uniform_buffer.raw_buffer(),
+            uniform_buffer.n_valid_uniforms(),
             uniform_id.as_ref(),
         );
 
@@ -48,14 +43,19 @@ impl UniformRenderBufferManager {
         }
     }
 
-    /// Creates the bind group entry for the uniform buffer,
-    /// assigned to the given binding.
+    /// Creates the bind group entry for the currently valid part
+    /// of the uniform buffer, assigned to the given binding.
+    ///
+    /// # Warning
+    /// This binding will be out of date as soon as the number of
+    /// valid uniforms changes.
     pub fn create_bind_group_entry(&self, binding: u32) -> wgpu::BindGroupEntry<'_> {
-        buffer::create_uniform_buffer_bind_group_entry(binding, self.uniform_render_buffer())
+        self.uniform_render_buffer()
+            .create_bind_group_entry(binding)
     }
 
     /// Returns the render buffer of uniforms.
-    pub fn uniform_render_buffer(&self) -> &RenderBuffer {
+    pub fn uniform_render_buffer(&self) -> &CountedRenderBuffer {
         &self.uniform_render_buffer
     }
 
@@ -77,24 +77,43 @@ impl UniformRenderBufferManager {
     {
         assert_eq!(U::ID, self.uniform_id);
 
-        if uniform_buffer.change() != CollectionChange::None {
-            let valid_bytes = bytemuck::cast_slice(uniform_buffer.valid_uniforms());
-            let n_valid_bytes = valid_bytes.len();
+        let change = uniform_buffer.change();
 
-            if n_valid_bytes > self.uniform_render_buffer.buffer_size() {
+        if change != CollectionChange::None {
+            let valid_uniforms = uniform_buffer.valid_uniforms();
+            let n_valid_uniforms = valid_uniforms.len();
+
+            let n_valid_uniform_bytes = mem::size_of::<U>()
+                .checked_mul(valid_uniforms.len())
+                .unwrap();
+
+            if self
+                .uniform_render_buffer
+                .bytes_exceed_capacity(n_valid_uniform_bytes)
+            {
                 // If the number of valid uniforms exceeds the capacity of the existing buffer,
                 // we create a new one that is large enough for all the uniforms (also the ones
                 // not currently valid)
-                self.uniform_render_buffer = RenderBuffer::new(
+                self.uniform_render_buffer = CountedRenderBuffer::new_uniform_buffer(
                     core_system,
-                    RenderBufferType::Uniform,
-                    bytemuck::cast_slice(uniform_buffer.raw_buffer()),
-                    n_valid_bytes,
+                    uniform_buffer.raw_buffer(),
+                    n_valid_uniforms,
                     self.uniform_id.as_ref(),
                 );
             } else {
-                self.uniform_render_buffer
-                    .update_valid_bytes(core_system, valid_bytes);
+                // We need to update the count of valid uniforms in the render buffer if it
+                // has changed
+                let new_count = if change == CollectionChange::Count {
+                    Some(Count::try_from(n_valid_uniforms).unwrap())
+                } else {
+                    None
+                };
+
+                self.uniform_render_buffer.update_valid_bytes(
+                    core_system,
+                    bytemuck::cast_slice(valid_uniforms),
+                    new_count,
+                );
             }
         }
         uniform_buffer.reset_change_tracking();
