c618ec8a1c5b18795f91883c5af06283f55b3878
Lars Frogner <lars_frogner@hotmail.com>
2023-02-03 20:56:30 +0100

    Make sure embedded count in a counted uniform render buffer is properly padded

--- src/rendering/buffer.rs
+++ src/rendering/buffer.rs
@@ -2,7 +2,7 @@
 
 use crate::rendering::{CoreRenderingSystem, MeshShaderInput};
 use bytemuck::Pod;
-use impact_utils::ConstStringHash64;
+use impact_utils::{Alignment, ConstStringHash64};
 use std::{
     mem,
     num::NonZeroU64,
@@ -60,6 +60,8 @@ pub struct RenderBuffer {
 pub struct CountedRenderBuffer {
     buffer: wgpu::Buffer,
     buffer_size: usize,
+    padded_count_size: usize,
+    item_size: usize,
     n_valid_bytes: AtomicUsize,
 }
 
@@ -255,9 +257,15 @@ impl RenderBuffer {
 }
 
 impl CountedRenderBuffer {
-    /// Creates a counted uniform render buffer initialized with
-    /// the given uniform data, with the first `n_valid_uniforms`
-    /// considered valid data.
+    /// Creates a counted uniform render buffer initialized with the given
+    /// uniform data, with the first `n_valid_uniforms` considered valid data.
+    ///
+    /// # Panics
+    /// - If `uniforms` is empty.
+    /// - If the size of a single uniform is not a multiple of 16 (the minimum
+    ///   required uniform alignment).
+    /// - If `n_valid_uniforms` exceeds the number of items in the `uniforms`
+    ///   slice.
     pub fn new_uniform_buffer<U>(
         core_system: &CoreRenderingSystem,
         uniforms: &[U],
@@ -267,11 +275,24 @@ impl CountedRenderBuffer {
     where
         U: UniformBufferable,
     {
+        // Uniforms have a minimum size of 16 bytes
+        let padded_count_size = 16;
+
+        let item_size = mem::size_of::<U>();
+
+        assert!(
+            Alignment::SIXTEEN.is_aligned(item_size),
+            "Tried to create uniform buffer with uniform size that \
+             causes invalid alignment (uniform buffer item stride \
+             must be a multiple of 16)"
+        );
+
         let count = Count::try_from(n_valid_uniforms).unwrap();
 
-        let n_valid_bytes = mem::size_of::<Count>()
-            .checked_add(mem::size_of::<U>().checked_mul(n_valid_uniforms).unwrap())
-            .unwrap();
+        let n_valid_bytes = Self::compute_size_including_count(
+            padded_count_size,
+            item_size.checked_mul(n_valid_uniforms).unwrap(),
+        );
 
         let bytes = bytemuck::cast_slice(uniforms);
 
@@ -280,30 +301,40 @@ impl CountedRenderBuffer {
             RenderBufferType::Uniform,
             count,
             bytes,
+            padded_count_size,
+            item_size,
             n_valid_bytes,
             &format!("{} uniform", label),
         )
     }
 
-    /// Creates a render buffer of the given type from the given slice of
-    /// bytes, and embed a count at the beginning of the buffer. Only the
-    /// first `n_valid_bytes` in the buffer (including the count) are considered
-    /// to actually represent valid data, the rest is just buffer filling
-    /// that gives room for writing a larger number of bytes than `n_valid_bytes`
-    /// into the buffer at a later point without reallocating.
+    /// Creates a render buffer of the given type from the given slice of bytes,
+    /// and embed a count at the beginning of the buffer. Only the first
+    /// `n_valid_bytes` in the buffer (including the count and its padding) are
+    /// considered to actually represent valid data, the rest is just buffer
+    /// filling that gives room for writing a larger number of bytes than
+    /// `n_valid_bytes` into the buffer at a later point without reallocating.
     ///
     /// # Panics
-    /// - If `n_valid_bytes` exceeds the combined size of the count and the
+    /// - If `bytes` is empty.
+    /// - If `n_valid_bytes` exceeds the combined size of the padded count and the
     ///   `bytes` slice.
     fn new(
         core_system: &CoreRenderingSystem,
         buffer_type: RenderBufferType,
         count: Count,
         bytes: &[u8],
+        padded_count_size: usize,
+        item_size: usize,
         n_valid_bytes: usize,
         label: &str,
     ) -> Self {
-        let buffer_size = Self::compute_size_including_count(bytes);
+        assert!(
+            !bytes.is_empty(),
+            "Tried to create empty counted render buffer"
+        );
+
+        let buffer_size = Self::compute_size_including_count(padded_count_size, bytes.len());
         assert!(n_valid_bytes <= buffer_size);
 
         let buffer_label = format!("{} render buffer", label);
@@ -312,52 +343,65 @@ impl CountedRenderBuffer {
             buffer_type,
             count,
             bytes,
+            padded_count_size,
             &buffer_label,
         );
 
         Self {
             buffer,
             buffer_size,
+            padded_count_size,
+            item_size,
             n_valid_bytes: AtomicUsize::new(n_valid_bytes),
         }
     }
 
-    /// Returns a slice of the underlying [`wgpu::Buffer`]
-    /// containing only valid bytes.
+    /// Returns the maximum number of items that can fit in the buffer (not
+    /// including the embedded count).
+    pub fn max_item_count(&self) -> usize {
+        self.buffer_size
+            .checked_sub(self.padded_count_size)
+            .unwrap()
+            .checked_div(self.item_size)
+            .unwrap()
+    }
+
+    /// Returns a slice of the underlying [`wgpu::Buffer`] containing only valid
+    /// bytes.
     pub fn valid_buffer_slice(&self) -> wgpu::BufferSlice<'_> {
         let upper_address = self.n_valid_bytes() as wgpu::BufferAddress;
         self.buffer.slice(..upper_address)
     }
 
-    /// Returns the number of bytes, starting from the beginning
-    /// of the buffer, that is considered to contain valid data
-    /// (this includes the count at the beginning of the buffer).
+    /// Returns the number of bytes, starting from the beginning of the buffer,
+    /// that is considered to contain valid data (this includes the padded count
+    /// at the beginning of the buffer).
     pub fn n_valid_bytes(&self) -> usize {
         self.n_valid_bytes.load(Ordering::Acquire)
     }
 
-    /// Whether the buffer is empty, meaning that it does not
-    /// contain any valid data apart from the count.
+    /// Whether the buffer is empty, meaning that it does not contain any valid
+    /// data apart from the count.
     pub fn is_empty(&self) -> bool {
-        self.n_valid_bytes() == mem::size_of::<Count>()
+        self.n_valid_bytes() == self.padded_count_size
     }
 
     /// Whether the given number of bytes would exceed the capacity of
-    /// the buffer (when the count at the beginning of the buffer is
+    /// the buffer (when the padded count at the beginning of the buffer is
     /// taken into account).
     pub fn bytes_exceed_capacity(&self, n_bytes: usize) -> bool {
-        mem::size_of::<Count>().checked_add(n_bytes).unwrap() > self.buffer_size
+        Self::compute_size_including_count(self.padded_count_size, n_bytes) > self.buffer_size
     }
 
     /// Queues a write of the given slice of bytes to the existing buffer,
-    /// starting just after the count at the beginning of the buffer. Any
+    /// starting just after the padded count at the beginning of the buffer. Any
     /// existing bytes in the buffer that are not overwritten are from then on
     /// considered invalid. If `new_count` is [`Some`], the count at the
     /// beginning of the buffer will be updated to the specified value.
     ///
     /// # Panics
-    /// If the combined size of the count and the slice of updated bytes exceeds
-    /// the total size of the buffer.
+    /// If the combined size of the padded count and the slice of updated bytes
+    /// exceeds the total size of the buffer.
     pub fn update_valid_bytes(
         &self,
         core_system: &CoreRenderingSystem,
@@ -365,7 +409,7 @@ impl CountedRenderBuffer {
         new_count: Option<Count>,
     ) {
         self.n_valid_bytes.store(
-            Self::compute_size_including_count(updated_bytes),
+            Self::compute_size_including_count(self.padded_count_size, updated_bytes.len()),
             Ordering::Release,
         );
 
@@ -375,18 +419,19 @@ impl CountedRenderBuffer {
             new_count,
             updated_bytes,
             self.buffer_size,
+            self.padded_count_size,
         );
     }
 
     /// Queues a write of the given slice of bytes to the existing buffer,
-    /// starting just after the count at the beginning of the buffer. The slice
-    /// must have the same size as the part of the buffer after the count. If
-    /// `new_count` is [`Some`], the count at the beginning of the buffer will
-    /// be updated to the specified value.
+    /// starting just after the padded count at the beginning of the buffer. The
+    /// slice must have the same size as the part of the buffer after the padded
+    /// count. If `new_count` is [`Some`], the count at the beginning of the
+    /// buffer will be updated to the specified value.
     ///
     /// # Panics
-    /// If the combined size of the count and the slice of updated bytes is not
-    /// the same as the total size of the buffer.
+    /// If the combined size of the padded count and the slice of updated bytes
+    /// is not the same as the total size of the buffer.
     pub fn update_all_bytes(
         &self,
         core_system: &CoreRenderingSystem,
@@ -394,22 +439,18 @@ impl CountedRenderBuffer {
         new_count: Option<Count>,
     ) {
         assert_eq!(
-            Self::compute_size_including_count(updated_bytes),
+            Self::compute_size_including_count(self.padded_count_size, updated_bytes.len()),
             self.buffer_size
         );
         self.update_valid_bytes(core_system, updated_bytes, new_count);
     }
 
     /// Creates a [`BindGroupEntry`](wgpu::BindGroupEntry) with the given
-    /// binding for the valid part of this counted uniform buffer.
+    /// binding for the full counted uniform buffer.
     pub fn create_bind_group_entry(&self, binding: u32) -> wgpu::BindGroupEntry<'_> {
         wgpu::BindGroupEntry {
             binding,
-            resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
-                buffer: self.buffer(),
-                offset: 0,
-                size: Some(NonZeroU64::new(self.n_valid_bytes() as u64).unwrap()),
-            }),
+            resource: self.buffer().as_entire_binding(),
         }
     }
 
@@ -418,8 +459,8 @@ impl CountedRenderBuffer {
         &self.buffer
     }
 
-    fn compute_size_including_count(bytes: &[u8]) -> usize {
-        mem::size_of::<Count>().checked_add(bytes.len()).unwrap()
+    fn compute_size_including_count(padded_count_size: usize, n_bytes: usize) -> usize {
+        padded_count_size.checked_add(n_bytes).unwrap()
     }
 
     fn create_initialized_counted_buffer_of_type(
@@ -427,20 +468,29 @@ impl CountedRenderBuffer {
         buffer_type: RenderBufferType,
         count: Count,
         bytes: &[u8],
+        padded_count_size: usize,
         label: &str,
     ) -> wgpu::Buffer {
         let usage = buffer_type.usage() | wgpu::BufferUsages::COPY_DST;
-        Self::create_initialized_counted_buffer(device, count, bytes, usage, label)
+        Self::create_initialized_counted_buffer(
+            device,
+            count,
+            bytes,
+            padded_count_size,
+            usage,
+            label,
+        )
     }
 
     fn create_initialized_counted_buffer(
         device: &wgpu::Device,
         count: Count,
         bytes: &[u8],
+        padded_count_size: usize,
         usage: wgpu::BufferUsages,
         label: &str,
     ) -> wgpu::Buffer {
-        let buffer_size = mem::size_of::<Count>().checked_add(bytes.len()).unwrap();
+        let buffer_size = Self::compute_size_including_count(padded_count_size, bytes.len());
 
         let buffer = device.create_buffer(&wgpu::BufferDescriptor {
             size: buffer_size as u64,
@@ -454,9 +504,9 @@ impl CountedRenderBuffer {
             let buffer_slice = buffer.slice(..);
             let mut mapped_memory = buffer_slice.get_mapped_range_mut();
 
-            // Write count to beginning, followed by actual data
+            // Write count to beginning, followed by actual data after the count padding
             mapped_memory[0..mem::size_of::<Count>()].copy_from_slice(bytemuck::bytes_of(&count));
-            mapped_memory[mem::size_of::<Count>()..].copy_from_slice(bytes);
+            mapped_memory[padded_count_size..].copy_from_slice(bytes);
         }
 
         buffer.unmap();
@@ -470,9 +520,10 @@ impl CountedRenderBuffer {
         count: Option<Count>,
         bytes: &[u8],
         buffer_size: usize,
+        padded_count_size: usize,
     ) {
-        // Write actual data starting just after the count
-        queue_write_to_buffer(queue, buffer, mem::size_of::<Count>(), bytes, buffer_size);
+        // Write actual data starting just after the padded count
+        queue_write_to_buffer(queue, buffer, padded_count_size, bytes, buffer_size);
 
         // Update the count if needed
         if let Some(count) = count {
