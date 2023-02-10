f6bb02e6800eca73706cfda886611255dcee8b3e
Lars Frogner <lars_frogner@hotmail.com>
2023-02-11 00:46:35 +0100

    Cull back faces by default

--- src/rendering/render_pass.rs
+++ src/rendering/render_pass.rs
@@ -612,7 +612,7 @@ impl RenderPassRecorder {
                 topology: wgpu::PrimitiveTopology::TriangleList,
                 strip_index_format: None,
                 front_face: wgpu::FrontFace::Ccw,
-                cull_mode: None, //Some(wgpu::Face::Back),
+                cull_mode: Some(wgpu::Face::Back),
                 polygon_mode: wgpu::PolygonMode::Fill,
                 unclipped_depth: false,
                 conservative: false,
