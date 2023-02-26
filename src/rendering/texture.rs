9450e37ae479073f97017def1dfb79d157dbd672
Lars Frogner <lars_frogner@hotmail.com>
2023-02-26 15:38:01 +0100

    Add shadow map texture type

--- src/rendering/texture.rs
+++ src/rendering/texture.rs
@@ -2,6 +2,8 @@
 
 mod depth;
 mod image;
+mod shadow_map;
 
 pub use self::image::ImageTexture;
 pub use depth::DepthTexture;
+pub use shadow_map::ShadowMapTexture;
---
+++ src/rendering/texture/shadow_map.rs
@@ -0,0 +1,128 @@
+//!
+
+use crate::rendering::CoreRenderingSystem;
+
+#[derive(Debug)]
+pub struct ShadowMapTexture {
+    _texture: wgpu::Texture,
+    view: wgpu::TextureView,
+    sampler: wgpu::Sampler,
+}
+
+impl ShadowMapTexture {
+    pub const FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Depth32Float;
+
+    pub fn new(core_system: &CoreRenderingSystem, width: u32, height: u32, label: &str) -> Self {
+        let device = core_system.device();
+
+        let texture_size = wgpu::Extent3d {
+            width,
+            height,
+            depth_or_array_layers: 1,
+        };
+
+        let texture = Self::create_texture(device, texture_size, label);
+
+        let view = Self::create_view(&texture);
+
+        let sampler = Self::create_sampler(device);
+
+        Self {
+            _texture: texture,
+            view,
+            sampler,
+        }
+    }
+
+    /// Returns a view into the shadow map texture.
+    pub fn view(&self) -> &wgpu::TextureView {
+        &self.view
+    }
+
+    /// Returns a sampler for the shadow map texture.
+    pub fn sampler(&self) -> &wgpu::Sampler {
+        &self.sampler
+    }
+
+    /// Creates the bind group layout entry for this texture type,
+    /// assigned to the given binding.
+    pub const fn create_texture_bind_group_layout_entry(
+        binding: u32,
+    ) -> wgpu::BindGroupLayoutEntry {
+        wgpu::BindGroupLayoutEntry {
+            binding,
+            visibility: wgpu::ShaderStages::FRAGMENT,
+            ty: wgpu::BindingType::Texture {
+                sample_type: wgpu::TextureSampleType::Depth,
+                view_dimension: wgpu::TextureViewDimension::D2,
+                multisampled: false,
+            },
+            count: None,
+        }
+    }
+
+    /// Creates the bind group layout entry for this texture's sampler
+    /// type, assigned to the given binding.
+    pub const fn create_sampler_bind_group_layout_entry(
+        binding: u32,
+    ) -> wgpu::BindGroupLayoutEntry {
+        wgpu::BindGroupLayoutEntry {
+            binding,
+            visibility: wgpu::ShaderStages::FRAGMENT,
+            // The sampler binding type must be consistent with the `filterable`
+            // field in the texture sample type.
+            ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Comparison),
+            count: None,
+        }
+    }
+
+    /// Creates the bind group entry for this texture,
+    /// assigned to the given binding.
+    pub fn create_texture_bind_group_entry(&self, binding: u32) -> wgpu::BindGroupEntry<'_> {
+        wgpu::BindGroupEntry {
+            binding,
+            resource: wgpu::BindingResource::TextureView(self.view()),
+        }
+    }
+
+    /// Creates the bind group entry for this texture's sampler,
+    /// assigned to the given binding.
+    pub fn create_sampler_bind_group_entry(&self, binding: u32) -> wgpu::BindGroupEntry<'_> {
+        wgpu::BindGroupEntry {
+            binding,
+            resource: wgpu::BindingResource::Sampler(self.sampler()),
+        }
+    }
+
+    fn create_texture(device: &wgpu::Device, size: wgpu::Extent3d, label: &str) -> wgpu::Texture {
+        device.create_texture(&wgpu::TextureDescriptor {
+            size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: Self::FORMAT,
+            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::RENDER_ATTACHMENT,
+            label: Some(label),
+            view_formats: &[],
+        })
+    }
+
+    fn create_view(texture: &wgpu::Texture) -> wgpu::TextureView {
+        texture.create_view(&wgpu::TextureViewDescriptor::default())
+    }
+
+    fn create_sampler(device: &wgpu::Device) -> wgpu::Sampler {
+        device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            compare: Some(wgpu::CompareFunction::LessEqual),
+            lod_min_clamp: 0.0,
+            lod_max_clamp: 100.0,
+            ..Default::default()
+        })
+    }
+}
