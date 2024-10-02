bcbbab1f1f9d0af4cbd3946bc8d07b57201542db
Lars Frogner <lars_frogner@hotmail.com>
2024-10-02 21:43:54 +0200

    Fix import

--- fuzz/fuzz_targets/voxel_object_generation.rs
+++ fuzz/fuzz_targets/voxel_object_generation.rs
@@ -1,7 +1,8 @@
 #![no_main]
 
 use impact::voxel::{
-    chunks::fuzz_test_voxel_object_generation, generation::fuzzing::ArbitrarySDFVoxelGenerator,
+    chunks::fuzzing::fuzz_test_voxel_object_generation,
+    generation::fuzzing::ArbitrarySDFVoxelGenerator,
 };
 use libfuzzer_sys::fuzz_target;
 
