d2720e784931fb7d398c56f19b2defe55b3a412f
Lars Frogner <lars_frogner@hotmail.com>
2025-07-07 10:40:29 +0200

    Fix old app name in binary

--- apps/basic_app/cli/src/main.rs
+++ apps/basic_app/cli/src/main.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 
 #[cfg(feature = "fuzzing")]
 define_ffi! {
-    name = ImpactGameFFI,
+    name = BasicAppFFI,
     lib_path_env = "BASIC_APP_LIB_PATH",
     lib_path_default = "../../../lib/libapp",
     run_with_config_at_path => unsafe extern "C" fn(*const u8, usize) -> i32,
@@ -13,14 +13,14 @@ define_ffi! {
 }
 #[cfg(not(feature = "fuzzing"))]
 define_ffi! {
-    name = ImpactGameFFI,
+    name = BasicAppFFI,
     lib_path_env = "BASIC_APP_LIB_PATH",
     lib_path_default = "../../../lib/libapp",
     run_with_config_at_path => unsafe extern "C" fn(*const u8, usize) -> i32,
 }
 
 #[derive(Debug, Parser)]
-#[command(about = "The Impact game", long_about = None)]
+#[command(about = "A basic Impact application", long_about = None)]
 struct Cli {
     #[command(subcommand)]
     command: Command,
@@ -28,7 +28,7 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    /// Run the game
+    /// Run the application
     Run {
         /// Path to RON configuration file to use
         #[arg(short, long)]
@@ -62,7 +62,7 @@ fn run(config_path: PathBuf) -> Result<()> {
     let config_path = config_path.to_string_lossy();
     let config_path_bytes = config_path.as_bytes();
 
-    ImpactGameFFI::call(
+    BasicAppFFI::call(
         |ffi| unsafe {
             error_code_to_result((ffi.run_with_config_at_path)(
                 config_path_bytes.as_ptr(),
@@ -76,7 +76,7 @@ fn run(config_path: PathBuf) -> Result<()> {
 #[cfg(feature = "fuzzing")]
 fn fuzz(test: FuzzTest, iterations: u64, seed: u64, verbose: bool) -> Result<()> {
     match test {
-        FuzzTest::CommandRoundtrip => ImpactGameFFI::call(
+        FuzzTest::CommandRoundtrip => BasicAppFFI::call(
             |ffi| unsafe {
                 error_code_to_result((ffi.fuzz_test_command_roundtrip)(
                     iterations as usize,
