1ebf6c6fd0a59c21a5381593943091916e308f26
Lars Frogner <lars_frogner@hotmail.com>
2025-09-09 15:56:48 +0200

    Combine standalone utility scripts into makefiles

--- apps/basic_app/clean_roc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cargo run --manifest-path tools/generate_roc/Cargo.toml --release -- clean -v -r -t roc_platform/api
--- apps/basic_app/generate_roc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cargo run --manifest-path tools/generate_roc/Cargo.toml --release -- generate-modules -t roc_platform/api -p pf -v
