219e8a4b7f053a2051dafa84e65ad64cf63b9f66
Lars Frogner <lars_frogner@hotmail.com>
2025-12-22 10:07:30 +0100

    Fix excessive precision warning

--- engine/crates/impact_math/src/consts/f32.rs
+++ engine/crates/impact_math/src/consts/f32.rs
@@ -3,5 +3,5 @@
 pub use std::f32::consts::*;
 
 pub const TWO_PI: f32 = TAU;
-pub const SQRT_3: f32 = 1.732050807568877293527446341505872367;
-pub const FRAC_1_SQRT_3: f32 = 0.577350269189625764509148780501957456;
+pub const SQRT_3: f32 = 1.7320508;
+pub const FRAC_1_SQRT_3: f32 = 0.57735026;
